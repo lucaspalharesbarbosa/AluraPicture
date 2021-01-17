6603fb44a53f79c56a47ecf845c949af3badcd22
Lucas <lucasp.b@hotmail.com>
2021-01-17 09:45:49 -0300

    - Implementado o component 'ap-photo' que rederiza uma foto de acordo com a url.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Alura Picture';
-  description = 'Leão';
-  url = 'https://i.pinimg.com/736x/6d/95/e3/6d95e3e6a442e5944d7411e10c5cced8.jpg';
 }
