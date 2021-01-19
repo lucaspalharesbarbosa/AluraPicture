ee7cc4501e8064eae683146d0e332b43ecb0356c
Lucas <lucasp.b@hotmail.com>
2021-01-18 22:33:34 -0300

    - Implementado o componente 'PhotoListComponent'.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,8 @@
-import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
-
-import { Photo } from './photos/photo/photo';
-import { PhotoService } from './photos/photo/photo.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  photos: Photo[] = [];
-
-  constructor(private photoService: PhotoService) { }
-
-  ngOnInit(): void {
-    this.photoService
-      .listFromUserName('flavio')
-      .subscribe(photos => this.photos = photos);
-  }
-}
+export class AppComponent { }
