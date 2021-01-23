225610881bc2b01803b3c17b07276be5f7708833
Lucas <lucasp.b@hotmail.com>
2021-01-23 18:30:30 -0300

    - Implementado o padrão debounce para filtrar as fotos somente após 500 milisegundos.

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from '../photo/photo';
 
@@ -11,10 +13,14 @@ import { Photo } from '../photo/photo';
 export class PhotoListComponent implements OnInit {
   photos: Photo[] = [];
   filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
 
   constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.photos = this.activatedRoute.snapshot.data['photos'];
+    this.debounce
+      .pipe(debounceTime(500))
+      .subscribe(filter => this.filter = filter);
   }
 }
