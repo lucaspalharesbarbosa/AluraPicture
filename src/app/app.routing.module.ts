dd7578d3be2d0d9dbc09476eff6413f7813dde17
Lucas <lucasp.b@hotmail.com>
2021-01-23 18:16:26 -0300

    - Implementado o resolver 'PhotoListResolver' para obter as photos no momento em que a rota for ativada para que o componete seja renderizado já com os dados disponíveis.

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -4,12 +4,24 @@ import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
+import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
 
 const routes: Routes = [
-    { path:'user/:userName', component: PhotoListComponent },
-    { path:'photo/add', component: PhotoFormComponent },
-    { path:'**', component: NotFoundComponent }
-
+    { 
+        path:'user/:userName',
+        component: PhotoListComponent,
+        resolve: { 
+            photos: PhotoListResolver 
+        }
+    },
+    {
+        path:'photo/add',
+        component: PhotoFormComponent
+    },
+    { 
+        path:'**',
+        component: NotFoundComponent
+    }
 ];
 
 @NgModule({
