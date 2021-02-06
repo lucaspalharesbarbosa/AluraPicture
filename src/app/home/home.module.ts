a738ac5fe74cd3ed1fa17a5ecd8ef883271c44a1
Lucas <lucasp.b@hotmail.com>
2021-02-05 22:20:17 -0300

    - Implementa

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
-import { SignInComponent } from './signin/signin.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+
 import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 import { RouterModule } from '@angular/router';
+import { SignUpComponent } from './signup/signup.component';
+import { SignInComponent } from './signin/signin.component';
 
 @NgModule({
-    declarations: [ SignInComponent ],
+    declarations: [
+        SignInComponent,
+        SignUpComponent
+    ],
     imports: [
         CommonModule,
+        FormsModule,
         ReactiveFormsModule,
         VMessageModule,
         RouterModule
