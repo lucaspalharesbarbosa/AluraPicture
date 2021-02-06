d609070290a2db2ee5cc0ee3c559d6563503711a
Lucas <lucasp.b@hotmail.com>
2021-02-06 14:29:19 -0300

    - Implementada a função de cadastrar usuários.

--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 import { lowerCaseValidator } from 'src/app/shared/validators/lower-case.validator';
+import { NewUser } from './new-user';
+import { SignUpService } from './signup.service';
 import { UserNotTakenValidatorService } from './user-not-taken.validator.service';
 
 @Component({
@@ -12,10 +15,16 @@ export class SignUpComponent implements OnInit {
 
     constructor(
         private formBuilder: FormBuilder,
-        private userNotTakenValidatorService: UserNotTakenValidatorService
+        private userNotTakenValidatorService: UserNotTakenValidatorService,
+        private signUpService: SignUpService,
+        private router: Router
     ) { }
 
     ngOnInit(): void {
+        this.createSignUpForm();
+    }
+
+    createSignUpForm(): void {
         this.signupForm = this.formBuilder.group({
             email: ['', [
                 Validators.required,
@@ -39,5 +48,15 @@ export class SignUpComponent implements OnInit {
                 Validators.maxLength(14)
             ]]
         })
+    }
+
+    signUp() {
+        const newUser = this.signupForm.getRawValue() as NewUser;
+        this.signUpService
+            .signUp(newUser)
+            .subscribe(
+                () => this.router.navigate(['']),
+                error => console.log(error)
+            );
     }
 }
--- src/app/home/signup/signup.service.ts
+++ src/app/home/signup/signup.service.ts
@@ -2,13 +2,19 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-const API = 'http://localhost:3000';
+import { NewUser } from './new-user';
+
+const API_URL = 'http://localhost:3000';
 
 @Injectable({ providedIn: 'root' })
 export class SignUpService {
     constructor(private httpClient: HttpClient) { }
 
     checkUserNameTaken(userName: string): Observable<Object> {
-        return this.httpClient.get(API + '/user/exists/' + userName)
+        return this.httpClient.get(API_URL + '/user/exists/' + userName)
+    }
+
+    signUp(newUser: NewUser) {
+        return this.httpClient.post(API_URL + '/user/signup', newUser)
     }
 }
