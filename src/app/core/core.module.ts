4971c376c20d61e3c3a6163ca8dc86ca4f2ff405
Lucas <lucasp.b@hotmail.com>
2021-02-09 21:21:17 -0300

    - Implementado o 'FooterComponent' que renderiza o rodapé da aplicação

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,10 +5,17 @@ import { RouterModule } from '@angular/router';
 
 import { RequestInterceptor } from './auth/request.interceptor';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-    declarations: [ HeaderComponent ],
-    exports: [ HeaderComponent ],
+    declarations: [
+        HeaderComponent,
+        FooterComponent
+    ],
+    exports: [
+        HeaderComponent,
+        FooterComponent
+    ],
     imports: [
         CommonModule,
         RouterModule
