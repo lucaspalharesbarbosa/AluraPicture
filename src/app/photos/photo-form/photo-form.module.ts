5f8a22c26366eba2af1f00409afb92132a51fc0e
Lucas <lucasp.b@hotmail.com>
2021-02-09 21:54:07 -0300

    - Implementado o template do 'PhotoFormComponent' para renderizar um formulário para fazer upload de fotos.

--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -2,11 +2,18 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { PhotoFormComponent } from './photo-form.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { VMessageModule } from '../../shared/components/vmessage/vmessage.module';
 
 @NgModule({
     declarations: [
         PhotoFormComponent
     ],
-    imports: [ CommonModule ]
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        VMessageModule,
+        FormsModule
+    ]
 })
 export class PhotoFormModule { }
