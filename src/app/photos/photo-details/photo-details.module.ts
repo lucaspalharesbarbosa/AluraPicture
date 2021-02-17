d0b9117b5d2f496d5f4e544ddfdcba5beab084d5
Lucas <lucasp.b@hotmail.com>
2021-02-16 22:25:22 -0300

    - Implementada a diretiva 'photo-owner-only' que esconde o elemento que ele esta inserida quando o usuário logado não é o mesmo que o da foto. - Inserida a diretiva 'photo-owner-only' no botão de remover foto.

--- src/app/photos/photo-details/photo-details.module.ts
+++ src/app/photos/photo-details/photo-details.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { PhotoModule } from "../photo/photo.module";
 import { PhotoDetailsComponent } from "./photo-details.component";
 import { PhotoCommentsComponent } from "./photo-comments/photo-comments.component";
-import { ReactiveFormsModule } from "@angular/forms";
 import { VMessageModule } from "../../shared/components/vmessage/vmessage.module";
+import { PhotoOwnerOnlyDirective } from "./photo-owner-only/photo-owner-only.directive";
 
 @NgModule({
     declarations: [
         PhotoDetailsComponent,
-        PhotoCommentsComponent
+        PhotoCommentsComponent,
+        PhotoOwnerOnlyDirective
     ],
     exports: [
         PhotoDetailsComponent,
---
+++ src/app/photos/photo-details/photo-owner-only/photo-owner-only.directive.ts
@@ -0,0 +1,34 @@
+import { Directive, Input, ElementRef, OnInit } from "@angular/core";
+import { Renderer } from "@angular/core";
+
+import { Photo } from "../../photo/photo";
+import { UserService } from "../../../core/user/user.service";
+
+@Directive({
+    selector: '[photo-owner-only]'
+})
+export class PhotoOwnerOnlyDirective implements  OnInit {
+    @Input() ownedPhoto: Photo;
+
+    constructor(
+        private elementRef: ElementRef<any>,
+        private renderer: Renderer,
+        private userService: UserService
+    ) { }
+
+    ngOnInit(): void {
+        this.userService.getUser()
+            .subscribe(user => {
+                const userIsLoggedOut = !user;
+                const photoIsNotTheUser = user.id != this.ownedPhoto.userId;
+                
+                if (userIsLoggedOut || photoIsNotTheUser) {
+                    this.hideElement();
+                }
+            })
+    }
+
+    private hideElement() {
+        this.renderer.setElementStyle(this.elementRef.nativeElement, 'display', 'none')
+    }
+}
