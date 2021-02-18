3ffbfd4cb9927c3c7a8df9a52df960153909e6ec
Lucas <lucasp.b@hotmail.com>
2021-02-17 22:12:17 -0300

    - Criada a rota 'not-found' e alterado para redirecionar para a rota 'not-found' ao tentar navegar numa rota 'photo/:photoId' passando um id inexistente.

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -36,8 +36,12 @@ const routes: Routes = [
 
     },
     { 
-        path:'**',
+        path:'not-found',
         component: NotFoundComponent
+    },
+    { 
+        path:'**',
+        redirectTo: 'not-found'
     }
 ];
 
