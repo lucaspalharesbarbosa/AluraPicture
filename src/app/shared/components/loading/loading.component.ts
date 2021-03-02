a5fcc2f8e6d1bbd13c4960eafffd4216ee975975
Lucas <lucasp.b@hotmail.com>
2021-03-01 21:31:39 -0300

    - Implementado o template do 'LoadingComponent'.

--- src/app/shared/components/loading/loading.component.ts
+++ src/app/shared/components/loading/loading.component.ts
@@ -8,7 +8,7 @@ import { LoadingService } from "./loading.service";
 @Component({
     selector: 'ap-loading',
     templateUrl: './loading.component.html',
-    styleUrls: [ './loading.component.css' ]
+    styleUrls: [ 'loading.component.css' ]
 })
 export class LoadingComponent implements OnInit {
     loading$: Observable<string>;
